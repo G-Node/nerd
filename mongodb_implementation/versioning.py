2e891e069a07bfbc33ade6e3441d9cb6229260ca
Lucas Mokrzycki <mokrzu@gmail.com>
2012-06-29 11:31:31 +0200

    put versioning method into class

--- mongodb_implementation/versioning.py
+++ mongodb_implementation/versioning.py
@@ -1,38 +1,43 @@
 from elements.section import Section
 from mapper import Mapper
 
-def save_section(section):
-	#create new section
-	s = Section()
+class Version:
 
-    s.name       = section.name
-    s.type_name  = section.type_name
-    s.reference  = section.reference
-    s.definition = section.definition
-    s.repository = section.repository
-    s.mapping    = section.mapping
-    s.link       = section.link
-    s.include    = section.include
-    s.isLatest   = True
+    def __init__(self)
+        self.m = Mapper()
 
-    section.isLatest = False
-
-    m = Mapper()
-    m._rewrite_properties(section, target_section)
-
-    # recursively add subsections
-    for section in source_section.sections:
-        s.subsections.append(self._save_section(section, None))
+    def save_section(self, section):
+    	#create new section
+    	s = Section()
+    
+        s.name       = section.name
+        s.type_name  = section.type_name
+        s.reference  = section.reference
+        s.definition = section.definition
+        s.repository = section.repository
+        s.mapping    = section.mapping
+        s.link       = section.link
+        s.include    = section.include
+        s.isLatest   = True
         
-        
-    s.sid()
-
-    # parent hash changed after subsections were added
-    # so parent hash need to be updated for every subsection
-    for section in s.subsections:
-        temp = Section.objects(object_id=section)[0]
-        temp.parent = s.sid()
-        temp.save()   
-
-    # save section in database
-    s.save()    
+        # now that section will not appear in search results
+        section.isLatest = False
+    
+        self.m._rewrite_properties(section, target_section)
+    
+        # recursively add subsections
+        for section in source_section.sections:
+            s.subsections.append(self.save_section(section))
+            
+            
+        s.sid()
+    
+        # parent hash changed after subsections were added
+        # so parent hash need to be updated for every subsection
+        for section in s.subsections:
+            temp = Section.objects(object_id=section)[0]
+            temp.parent = s.sid()
+            temp.save()   
+    
+        # save section in database
+        s.save()    
